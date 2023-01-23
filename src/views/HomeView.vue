b871a4932191a9277f428ff7686f311b998b0c0e
Mosabberul Islam <mosabberulislam55@gmail.com>
2023-01-23 22:31:32 +0600

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click me</button>
+
+    <input type="text" v-model="name" />
+  </div>
+</template>
+
+<script>
+
+import {ref} from "vue";
+
+export default {
+  name: 'HomeView',
+  setup() {
+    // console.log(this)
+
+    const p = ref(null)
+
+    const name = ref('Mario')
+    const age = ref(30)
+    const handleClick = () => {
+      name.value = 'John'
+      age.value = 50
+      console.log(name, name.value)
+      // console.log(p, p.value)
+      // p.value.classList.add('red')
+      // p.value.textContent = 'My name'
+    }
+    return { name, age, handleClick, p }
+  }
+}
+</script>
